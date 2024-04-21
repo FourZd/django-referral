3d55a5846554abb0e161cf1a5ffe0f1ff8b68333
FourZd <shogo.ryaskov@gmail.com>
2024-04-22 01:49:26 +0300

    add hosts

--- referral_project/settings.py
+++ referral_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+0aax()bw*(ic#70xra4*mn9ps#)i_c9ij_bsv0$=%pp%1h1l2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
